fc9c4ca7f65f179de65ca8636ad808c8977b543d
YLokma <94978677+YLokma@users.noreply.github.com>
2022-04-25 13:24:04 +0200

    Clearer output

--- Logic_Gates_int.py
+++ Logic_Gates_int.py
@@ -8,9 +8,14 @@ while gate_count < 1 or gate_count > 9:
 	gate_count = int(input('Invalid number of gates, try again \n'))
 for point in Table:
 	if counter < gate_count:
+		turn = str(counter + inputs_no + 1) + '='
+		print('Enter the small statement in the form (N GATE M) or (NOT N)')
 		print('Choose the gates from')
 		print('0:NOT \t 1:AND \t 2:OR \t 3:NAND \t 4:NOR \t 5:XOR')
-		small_statement = str(input('Enter the small statement in the form (123) where 2 is the gate or (01) for NOT 1 \n'))
+		if counter == gate_count - 1:
+			small_statement = str(input('X = '))
+		else:	
+			small_statement = str(input(str(counter + inputs_no + 1) + ' = '))		
 		small_statement = small_statement.split()
 		if len(small_statement) == 1:
 			if small_statement[0][0] == '0':
@@ -25,26 +30,50 @@ for point in Table:
 				small_statement[i] = int(small_statement[i])
 		if len(small_statement) == 2:
 			while small_statement[0] != 0:
-				small_statement = str(input('Invalid gate number, re-enter the small statement in the form (123) \n'))
+				print('Re-enter the small statement in the form (N GATE M) or (NOT N)')
+				print('Choose the gates from')
+				print('0:NOT \t 1:AND \t 2:OR \t 3:NAND \t 4:NOR \t 5:XOR')
+				if counter == gate_count - 1:
+					small_statement = str(input('X = '))
+				else:	
+					small_statement = str(input(str(counter + inputs_no + 1) + ' = '))	
 				small_statement = small_statement.split()
 				for i in range(0,2):
 					small_statement.append(int(small_statement[0][i]))
 				small_statement.pop(0)
 			while small_statement[1]-1 not in range(0,len(Table)):
-				small_statement = str(input('Re-enter the small statement in the form (123) where 2 is the gate \n'))
+				print('Re-enter the small statement in the form (N GATE M) or (NOT N)')
+				print('Choose the gates from')
+				print('0:NOT \t 1:AND \t 2:OR \t 3:NAND \t 4:NOR \t 5:XOR')
+				if counter == gate_count - 1:
+					small_statement = str(input('X = '))
+				else:	
+					small_statement = str(input(str(counter + inputs_no + 1) + ' = '))	
 				small_statement = small_statement.split()
 				for i in range(0,3):
 					small_statement.append(int(small_statement[0][i]))
 				small_statement.pop(0)
 		else:
 			while small_statement[1] > 5 or small_statement[1] < 0:
-				small_statement = input('Invalid gate number, re-enter the small statement in the form (123) \n')
+				print('Re-enter the small statement in the form (N GATE M) or (NOT N)')
+				print('Choose the gates from')
+				print('0:NOT \t 1:AND \t 2:OR \t 3:NAND \t 4:NOR \t 5:XOR')
+				if counter == gate_count - 1:
+					small_statement = str(input('X = '))
+				else:	
+					small_statement = str(input(str(counter + inputs_no + 1) + ' = '))	
 				small_statement = small_statement.split()
 				for i in range(0,3):
 					small_statement.append(int(small_statement[0][i]))
 				small_statement.pop(0)
 			while small_statement[0]-1 not in range(0,len(Table)) or small_statement[2]-1 not in range(0,len(Table)):
-				small_statement = input('Re-enter the small statement in the form (123) where 2 is the gate \n')
+				print('Re-enter the small statement in the form (N GATE M) or (NOT N)')
+				print('Choose the gates from')
+				print('0:NOT \t 1:AND \t 2:OR \t 3:NAND \t 4:NOR \t 5:XOR')
+				if counter == gate_count - 1:
+					small_statement = str(input('X = '))
+				else:	
+					small_statement = str(input(counter + gate_count))
 				small_statement = small_statement.split()
 				for i in range(0,3):
 					small_statement.append(int(small_statement[0][i]))
@@ -61,12 +90,14 @@ for point in Table:
 			else: 
 				print('Error')
 				break
+		print('')
 		if counter < gate_count - 1:
 			for i in range(0,len(Table)):
 				print(i+1,'=',Table[i])
 		else:
 			for i in range(0,len(Table)-1):
 				print(i+1,'=',Table[i])
+		print('')
 	else: break
 	counter += 1
-print('\n\t\t\t X = ', Table[-1], '\n')
+print('\t\t X = ', Table[-1], '\n')

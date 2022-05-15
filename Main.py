ad7e1bb0d570036a22d6ba2e10079ca1203f6bfe
YLokma <youssoflokma@gmail.com>
2022-05-15 23:19:43 +0200

    clearer choices for input methods

--- Main.py
+++ Main.py
@@ -3,9 +3,9 @@ from Functions import *
 print("\nWelcome to our logic gates project, if you haven't already, please check out our GitHub for instructions")
 print('https://github.com/YLokma/Logic_Gates \n')
 print('Choose the type of input you prefer')
-print('1: type using numbers only')
-print('2: type gates as text and the points as numbers')
-print('3: type using text and spaces only')
+print('1: type using numbers only (112)')
+print('2: type gates as text and the points as numbers (1 and 2)')
+print('3: type using text and spaces only (a and b)')
 
 input_type = numerically_validate('input type',3,1)
 
